5fb855bd97c6bc0bae4c714134c3898ed94f7aa0
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2023-04-16 18:28:34 +0200

    Add documentation for the functions.

--- mypopen.h
+++ mypopen.h
@@ -23,17 +23,60 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+   A struct pipe_set.
+   This a struct returned by mypopen calls. The function allocates them with
+   malloc, and mypclose releases it with free.
+ */
 struct pipe_set {
-	pid_t pid;
-	FILE *fd[3];
+	pid_t pid;    /**< PID of the new process. */
+	FILE *fd[3];  /**< Array of file descriptors; null when not connected. */
 };
 
+/**
+   Function to create a new process.
+
+   Depending of the modes argument the function creates a new process with
+   fork+exec, and then returns a struct pipe_set where fd are connected to the
+   process' stdin, stdout and stderr.
+   \param modes is an unsigned integer which works like a mask of the connected
+   outputs.
+   1: fd[0] is connected to the process stdin, and it is possible to
+   send commands to the input by writing to it.
+   2: fd[1] is connected to process stdout, and it is possible to read the
+   processes' output.
+   4: fd[2] is connected to process stderr.
+   To connect all the descriptors use: (1|2|4) => 7.
+   \param cmd a string with the full command line to start the new process. This
+   arguments are passed to execv.
+   \return a pipe_set object with the pid and file descriptors information based
+   on mask.
+ */
 struct pipe_set *mypopen(const unsigned int modes, char *const cmd[]);
 
+/**
+   Close the connection to the process and release the pipes object.
+   \param the process descriptor as returned by mypopen. This function calls
+   free to release the heap allocated memory.
+ */
 int mypclose(struct pipe_set *pipes);
 
+/**
+   Create a process using mkfifo instead of fork+exec
+
+   mkfifo creates a physical file descriptor in the filesystem which is possible
+   to write like: 'echo "bla" > file'; and the command will be passed to the
+   processes standard input.
+   \param pipename The path of the desired file descriptor
+   \param cmd similar to similar argument in mypopen.
+   \return a pipe_set object with the pid and file descriptors information. Only
+   fd[0] is set in this function.
+ */
 struct pipe_set *mymkfifo(const char pipename[], char *const cmd[]);
 
+/**
+   Wait for the process described in pipes to finish.
+ */
 void mywaitpid(struct pipe_set *pipes);
 
 #endif // MYPOPEN_H
